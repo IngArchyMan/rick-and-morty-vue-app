73a0449abe314139b599d9ded8fa339a01fead84
Ing. Archy <dsbecerrac@smarthinkscorp.com>
2021-09-05 23:37:32 -0500

    primer proyecto en vue

---
+++ src/components/CardCharacter.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="character">
+      <img :src="character.image" :alt="character.name">
+      <div class="character__info">
+          <h3>{{character.name}}</h3>    
+          <div class="status">
+              <span
+                  :class="
+                    character.status == 'Alive' ?  'alive' : 
+                    character.status == 'Dead' ? 'dead' : 
+                    'default'
+                   "
+              ></span>
+          </div>
+          <div class="species">
+              <span>
+                  <strong>Species:</strong> {{character.species}}
+              </span>
+          </div>
+          <div class="origin">
+              <span>
+                  <strong>Origin:</strong> {{character.origin.name}}
+              </span>
+          </div>
+          <div class="location">
+              <span>
+                  <strong>Location:</strong>  {{character.location.name}}
+              </span>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['character']
+}
+</script>
+
+<style>
+    .characters {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        gap: 3rem;
+        margin: 3rem 0;
+    }
+</style>
---
+++ src/components/FilterByName.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="search">
+      <input type="text" placeholder="Search By Name" @keyup="filter()" v-model="name">
+  </div>
+</template>
+
+<script>
+import {ref} from 'vue'
+import {useStore} from 'vuex'
+
+export default {
+    setup(){
+        const store = useStore()
+        const name = ref('')
+
+        const filter = (()=>{
+            store.dispatch('FilterByName', name.value)
+        })
+
+        return {
+            name,
+            filter
+        }
+    }
+
+}
+</script>
+
+<style>
+.search{
+    width: 400px;
+    margin: 3rem auto 0;
+}
+
+.search input{
+    width: 400px;
+    border: 1px solid var(--text-gray);
+    background-color: transparent;
+    border-radius: 10px;
+    padding: 0.5rem;
+    color: var(--text-white);
+}
+
+</style>
